b778c679a98048fdf0a57e0238422372624749e3
Amir P <apourrezaei@gmail.com>
2019-04-03 17:34:55 -0400

    Functional 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,27 @@
 var q;
 var answer;
-var score=0;
+var score = 0;
+var next = 0;
 
 var model = [{
-    Question: "What is georges middle name",
+    Question: "What is georges middle name?",
     Answers: ["Bob", "John", "Luis", "Phil"],
     Correct: 2
-}];
+}, {
+    Question: "Which president was martin van buren?",
+    Answers: ["Eighth", "First", "Third", "Tenth"],
+    Correct: 0
+}
+];
+var max = model.length;
+
+
 
 
 
 $(document).ready(function () {
-    GenerateQuestion();
+    
+    GenerateQuestion(next);
 
     $("#start").click(QuizTimer);
     $("#stop").click(StopTimer)
@@ -20,20 +30,22 @@ $(document).ready(function () {
 
 
 
-function GenerateQuestion() {
-    $("#question").text(model[0].Question);
-    for (var i = 0; i < model[0].Answers.length; i++) {
-        var tempAnswer = model[0].Answers[i];
+function GenerateQuestion(q) {
+
+
+    $("#question").text(model[q].Question);
+    for (var i = 0; i < model[q].Answers.length; i++) {
+        var tempAnswer = model[q].Answers[i];
         var choiceInput = "<input type=\"radio\" name=\"answer\" value=\"" + tempAnswer + "\">" + tempAnswer + "</input>";
         $("#answerChoice").append(choiceInput);
     }
-    answer = model[0].Answers[model[0].Correct];
+    answer = model[q].Answers[model[q].Correct];
     console.log(answer);
 }
 
 function QuizTimer() {
     var t = 10;
-     q = setInterval(function() {
+    q = setInterval(function () {
         $("#Timer").text(t);
         t--;
     }, 1000);
@@ -45,13 +57,23 @@ function StopTimer() {
 
 function CheckAnswer() {
     var radioValue = $("input[name='answer']:checked").val();
-    if(radioValue===answer) {
+    if (radioValue === answer) {
         console.log("Correct!");
         score++;
         $("#score").text(score);
     }
     else {
         console.log("Incorrect!");
+    }
+    $("#answerChoice").empty();
+    $("#question").empty();
+    next++;
+    if(next < max)
+    {
+        GenerateQuestion(next);
+    }   
+    else {
+        $("#message").text("Game Over!");
     }
 
 }
