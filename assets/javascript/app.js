743fd280309d38634b02600fc286f36bc48a097f
Amir P <apourrezaei@gmail.com>
2019-04-03 14:49:46 -0400

    Stable w submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,6 @@
 $(document).ready(function () {
-    $("#question").text(model[0].Question);
-    for (var i = 0; i < model[0].Answers.length; i++) {
-        var tempAnswer = model[0].Answers[i];
-        var choiceInput = 
-        "<input type=\"radio\" name=\"answer\" value=\"" + tempAnswer + "\">" + tempAnswer + "</input>";
-        
-        $("#answerChoice").append(choiceInput);
+    GenerateQuestion();
 
-    }
     $("#start").click(QuizTimer);
     $("#stop").click(StopTimer)
 
@@ -17,9 +10,21 @@ $(document).ready(function () {
 
 var model = [{
     Question: "What is georges middle name",
-    Answers: ["Bob", "John", "Luis", "Phil"]
+    Answers: ["Bob", "John", "Luis", "Phil"],
+    Correct: 2
 }];
 var q;
+
+function GenerateQuestion() {
+    $("#question").text(model[0].Question);
+    for (var i = 0; i < model[0].Answers.length; i++) {
+        var tempAnswer = model[0].Answers[i];
+        var choiceInput = "<input type=\"radio\" name=\"answer\" value=\"" + tempAnswer + "\">" + tempAnswer + "</input>";
+        $("#answerChoice").append(choiceInput);
+    }
+    var answer = model[0].Answers[model[0].Correct];
+    console.log(answer);
+}
 
 function QuizTimer() {
     var t = 10;
